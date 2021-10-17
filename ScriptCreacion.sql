6c16c0abd7822ac675f27da5ff9504b294e6b033
Juandi2200 <57282878+Juandi22001@users.noreply.github.com>
2021-10-17 17:37:03 -0600

    Update ScriptCreacion.sql

--- ScriptCreacion.sql
+++ ScriptCreacion.sql
@@ -35,5 +35,105 @@ CREATE TABLE
 );
 
 
+create table Pais (
+    ID_PAIS  INT AUTO_INCREMENT,
+  nombre_pais VARCHAR(100),
+  PRIMARY KEY (ID_PAIS)
 
-select * from temporal;
+
+
+);
+
+
+
+create table Region (
+    ID_Region  INT AUTO_INCREMENT,
+  nombre_Region VARCHAR(100),
+  ID_PAIS INT,
+  PRIMARY KEY (ID_Region),
+  FOREIGN KEY  (ID_PAIS) REFERENCES  PAIS(ID_PAIS)
+
+
+
+);
+create table Departamento (
+    ID_Departamento  INT AUTO_INCREMENT,
+  nombre_Departamento VARCHAR(100),
+  ID_REGION INT,
+  PRIMARY KEY (ID_Departamento),
+  FOREIGN KEY  (ID_REGION) REFERENCES  region (ID_region)
+
+
+
+);
+
+create table Municipio (
+    ID_Municipio  INT AUTO_INCREMENT,
+  nombre_Municipio VARCHAR(100),
+  ID_departamento INT,
+  PRIMARY KEY (ID_Municipio),
+  FOREIGN KEY  (ID_departamento) REFERENCES  departamento (ID_Departamento)
+
+
+
+);
+create table Partido (
+    ID_partido  INT AUTO_INCREMENT,
+  Nombre_Partido VARCHAR(100),
+  Partido VARCHAR(100),
+
+  ID_Pais INT,
+  PRIMARY KEY (ID_partido),
+  FOREIGN KEY  (ID_Pais) REFERENCES  Pais (ID_Pais)
+
+
+
+);
+
+create table Tipo_Eleccion (
+    ID_TipoEleccion  INT AUTO_INCREMENT,
+ TipoEleccion VARCHAR(100),
+  PRIMARY KEY (ID_TipoEleccion)
+
+
+
+);
+create table Eleccion
+(
+    ID_Eleccion  INT AUTO_INCREMENT,
+    AÑO_ELECCION VARCHAR(100),
+    TipoEleccion INT,
+    id_pais      INT,
+
+
+    PRIMARY KEY (ID_Eleccion),
+
+    Foreign key (TipoEleccion) references Tipo_Eleccion(ID_TipoEleccion),
+    Foreign key (id_pais) references Pais(id_pais)
+
+
+);
+
+use Proyecto2Bases;
+
+create table Resultado
+(
+    Id_Resultado   INT AUTO_INCREMENT,
+    Analafabetas   INT,
+    Alfabetas      INT,
+    Primaria       INT,
+    Nivel_Medio    INT,
+    Universitarios INT,
+    Id_Municipio   INT,
+    Id_Eleccion    INT,
+
+    PRIMARY KEY (Id_Resultado),
+
+    Foreign key (Id_Eleccion) references eleccion (ID_Eleccion),
+    Foreign key (Id_Municipio) references municipio (id_municipio)
+
+
+);
+
+
+
