5e6d670fa30633d70a9f874ea06e483f4710b4c3
Juandi2200 <57282878+Juandi22001@users.noreply.github.com>
2021-10-17 16:59:19 -0600

    Iniciando

---
+++ ScriptCreacion.sql
@@ -0,0 +1,39 @@
+
+use Proyecto2Bases;
+CREATE TABLE
+    temporal
+(
+
+
+    nombre_eleccion varchar(100),
+
+    anio_eleccion   varchar(100),
+
+    pais            varchar(100),
+
+    region          varchar(100),
+
+    Departamento    varchar(100),
+
+    Municipio       varchar(100),
+    Partido         varchar(100),
+
+    Nombre_paritdo  varchar(100),
+
+    Sexo            varchar(100),
+
+    Raza            varchar(100),
+
+    Analfabetos     int,
+    Alfabetos       int,
+    Primaria        int,
+
+    Nivel_Medio     int,
+    Universitarios  int
+
+
+);
+
+
+
+select * from temporal;
